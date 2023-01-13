e62f200d63f1b6554f2479dd886342dd314842cd
ideuxist <ideuxist@gmail.com>
2023-01-13 14:09:01 +0900

    board-show.html 페이지 작성

--- js/validate/check-validation.js
+++ js/validate/check-validation.js
@@ -4,32 +4,35 @@ const memberValidation = {
         //영문으로 시작하고 영 숫자 허용 5자리 이상 20자리 이하-
         format : { pattern: "(?=[\d]*[a-zA-Z])[a-zA-Z0-9]{5,20}" }
     },
-    memberPassword : {
+    memberPw : {
         presence : true,
         format : { pattern: "(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,20}$" }
     },
-    memberPasswordCheck : {
+    memberPwCheck : {
         presence : true,
         equality : "memberPassword"
     },
-    memberName : {
+    name : {
         presence : true,
         format : { pattern : "^[기-힣]{2,8}" },
     },
-    memberNickname : {
+    nickName : {
         presence : true,
         format : {pattern : "[기-힣a-zA-Z0-9]{1,20}" },
     },
-    memberPhone : {
-      presence : true,
-      format : { pattern : "^((010-[0-9]{4})|((01[16789]{1}|02|0[3-9]{1}[0-9]{1})-[0-9]{3,4}))-[0-9]{4}$" }
+    phoneNumber : {
+        presence : true,
+        format : { pattern : "^((010-[0-9]{4})|((01[16789]{1}|02|0[3-9]{1}[0-9]{1})-[0-9]{3,4}))-[0-9]{4}$" }
     },
-    memberEmail : {
+    email : {
         presence : true,
         format : { pattern : "^[a-zA-Z0-9]([-_\.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_\.]?[a-zA-Z0-9])*\.[a-zA-Z0-9]{2,3}$" }
     }
 
 }
+
+const allConstraint = new Map();
+allConstraint.set('memberValidation',memberValidation)
 
 //유효성 체크 항목 is-invalid 초기화
 function removeIsInvalidInClass() {
@@ -51,15 +54,16 @@ function addIsInvalidInClass(result) {
 
 }
 
-document.getElementById('memberJoinForm').addEventListener('submit',function(event){
+document.querySelector('.need-validation-form').addEventListener('submit',function(event){
     event.preventDefault();
     removeIsInvalidInClass();
-    const result = validate(this,memberValidation);
+    const result = validate(this,allConstraint.get(this.dataset.constraint));
 
     if(result) {
         addIsInvalidInClass(result);
     }else {
         this.submit();
     }
+
 });
 
